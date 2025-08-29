5a791d6bc2eba626e316cbd0e7362c9b4ce8b09d
rjb-21 <radek0101@gmail.com>
2025-08-29 09:53:50 +0200

    backend files tested- test bucket added

---
+++ infra/envs/dev/main.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_version = ">= 1.5"
+
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = ">= 5.0"
+    }
+  }
+}
+
+provider "google" {
+  project = var.project_id
+  region  = var.region
+}
+
+resource "google_storage_bucket" "test_bucket" {
+  name          = "rjb21-tf-test-bucket"
+  location      = "EU"
+  force_destroy = true
+}
---
+++ infra/envs/dev/variables.tf
@@ -0,0 +1,10 @@
+variable "project_id" {
+  type    = string
+  default = "terraform-470506"
+}
+
+variable "region" {
+  type    = string
+  default = "europe-central2"
+}
+

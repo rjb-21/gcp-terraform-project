edcfb3f5f6887724f82fbccc0c858a77e596c585
rjb-21 <radek0101@gmail.com>
2025-08-29 08:56:54 +0200

    bootstrap added- state bucket, API

---
+++ bootstrap/main.tf
@@ -0,0 +1,49 @@
+terraform {
+  required_version = ">= 1.6"
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = ">= 5.0"
+    }
+  }
+}
+
+provider "google" {
+  project = var.project_id
+  region  = var.region
+}
+
+# APIs
+resource "google_project_service" "apis" {
+  for_each = toset([
+    "run.googleapis.com",
+    "artifactregistry.googleapis.com",
+    "sqladmin.googleapis.com",
+    "secretmanager.googleapis.com",
+    "cloudresourcemanager.googleapis.com",
+    "iam.googleapis.com",
+    "iamcredentials.googleapis.com",
+    "monitoring.googleapis.com",
+    "pubsub.googleapis.com",
+  ])
+  service = each.key
+}
+
+# Bucket for statefile
+resource "google_storage_bucket" "tf_state" {
+  name                        = var.state_bucket_name
+  location                    = var.region
+  uniform_bucket_level_access = true
+
+  versioning {
+    enabled = true
+  }
+
+  lifecycle_rule {
+    action { type = "Delete" }
+    condition { num_newer_versions = 10 }
+  }
+}
+
+
+
---
+++ bootstrap/variables.tf
@@ -0,0 +1,15 @@
+variable "project_id" {
+  type    = string
+  default = "terraform-470506"
+}
+
+variable "region" {
+  type    = string
+  default = "europe-central2"
+}
+
+variable "state_bucket_name" {
+  type = string
+  default = "terraformhelloworldproject-tf-state-rjb21"
+
+}

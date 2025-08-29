6855743309e9c1d7658cbaf3af486d108dbc5d91
rjb-21 <radek0101@gmail.com>
2025-08-29 10:06:43 +0200

    dev: main file edited, var added

--- infra/envs/dev/main.tf
+++ infra/envs/dev/main.tf
@@ -13,9 +13,3 @@ provider "google" {
   project = var.project_id
   region  = var.region
 }
-
-resource "google_storage_bucket" "test_bucket" {
-  name          = "rjb21-tf-test-bucket"
-  location      = "EU"
-  force_destroy = true
-}
--- infra/envs/dev/variables.tf
+++ infra/envs/dev/variables.tf
@@ -8,3 +8,8 @@ variable "region" {
   default = "europe-central2"
 }
 
+variable "app_name" {
+  description = "Test application name"
+  type        = "string
+  default     = 'terraform-helloworld-app"
+}
